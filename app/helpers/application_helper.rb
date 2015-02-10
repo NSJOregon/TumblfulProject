897036dc973ac098939c0b1a3720edffc5d1ce67
NSJOregon <nsjoregon@gmail.com>
2015-02-10 05:29:49 +0000

    first commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,48 @@ module ApplicationHelper
   # authenticated user is already following the user passed, an unfollow (DELETE) form
   # will be generated. Otherwise, a follow (CREATE) form will be generated.
   #
+  
+  # more info: http://api.rubyonrails.org/classes/ActionView/Helpers/FormBuilder.html
+#
+# This class inherits from the default FormBuilder class and adds two public methods
+# for displaying errors related to specific fields.
+#
+class InlineErrorsBuilder < ActionView::Helpers::FormBuilder
+
+  # Generates HTML for displaying errors related to the attribute passed as 'meth'
+  #
+  def errors_for(meth, options = {})
+    @template.content_tag :p, @object.errors[meth].join('<br>').html_safe, :class => 'help-block' if has_errors?(meth)
+  end
+
+  # Adds the 'has-error' class to the list of other classes passed if there are
+  # errors on the attribute passed as 'meth'
+  #
+  def validation_class(meth, *klasses)
+    klasses << 'has-error' if has_errors?(meth)
+    klasses.compact.join(' ').html_safe
+  end
+
+  private
+
+  # Determines if there are errors on the attribute passed as 'meth'. Includes protection
+  # against edge cases where there is no object present.
+  #
+  def has_errors?(meth)
+    @object.present? ? @object.errors[meth].present? : false
+  end
+end
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   def follow_link(user)
     follow = Follow.where(:user => current_user, :following => user)
     if follow.exists?
@@ -78,4 +120,9 @@ module ApplicationHelper
     html = content_tag :strong, flash[type]
     html.html_safe
   end
+      
+      
+      
+      
+      
 end
